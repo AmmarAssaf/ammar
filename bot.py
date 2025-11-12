9fb857dadd6e08676d9a14c936eeee3393383a7f
AmmarAssaf <ammarasdassaf@gmail.com>
2025-11-12 09:19:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
 
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    result = 5 + 3
+    return f'<h1>نتيجة الجمع: 5 + 3 = {result}</h1>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
