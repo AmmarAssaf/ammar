1876c585c59ff5ea7f4e4c1e481c887a9665e567
AmmarAssaf <ammarasdassaf@gmail.com>
2025-11-12 08:20:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,7 @@
-# simplest.py
-result = 5 + 3
-print(f"5 + 3 = {result}")
+# add.py
+def add_numbers(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    result = add_numbers(5, 3)
+    print(f"5 + 3 = {result}")
